1a09ee4569bd99a8509ee14d80ba9b588c4126f0
aightech <aiiight.tech@gmail.com>
2022-04-10 16:20:10 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,13 @@ int main()
   std::cout <<"|";
   for(int i =0; i<js.getNbButtons(); i++)
     std::cout << ((i>=10)?"":" ") << i << "|";
-  //std::cout << std::endl;
+
   
   std::cout <<"| ";
   for(int i =0; i<js.getNbAxis(); i++)
     std::cout << i << "\t| ";
   std::cout << std::endl;
  
-
-  // for(;;)
-  //   {
-  //     std::cout <<"|";
-  //     for(int i =0; i<8*js.getNbAxis(); i++)
-  // 	std::cout << " ";
-  //     std::cout <<"\xd|";
-  //     for(int i =0; i<js.getNbAxis(); i++)
-  // 	std::cout << ((js.joystickValue(i)<0)?"":" ") << js.joystickValue(i) << "\t|";
-  //     std::cout << "\xd" << std::flush;
-      
-  //   }
   for(;;)
     {
       std::cout <<"|";
@@ -34,8 +22,7 @@ int main()
       std::cout <<"\xd| ";
       for(int i =0; i<js.getNbButtons(); i++)
 	std::cout << js.buttonPressed(i)  << "| ";
-      //std::cout << "\xd" << std::flush;
-
+      
       std::cout <<"\b|";;
       for(int i =0; i<js.getNbAxis(); i++)
   	std::cout << ((js.joystickValue(i)<0)?"":" ") << js.joystickValue(i) << "\t|";
